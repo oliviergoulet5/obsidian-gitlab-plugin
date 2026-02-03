d99232d0dcd01605b8ec9a859cebdb42535cc3a5
Olivier Goulet <olivier_goulet@icloud.com>
2026-02-02 22:46:32 -0500

    feat: display issue embed

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin} from "obsidian";
+import { App, Editor, MarkdownView, requestUrl, Modal, Notice, Plugin, addIcon } from "obsidian";
 import { DEFAULT_SETTINGS, GitLabPluginSettings, GitLabSettingTab } from "./settings";
 
 enum GitLabResource {
@@ -34,10 +34,11 @@ export default class GitLabPlugin extends Plugin {
 
   private async processAnchor(anchorElement: HTMLAnchorElement): Promise<void> {
     const url = new GitLabURL(anchorElement.href);
+    const embedParentElement = anchorElement.parentElement as HTMLElement;
 
     switch (url.resource) {
       case GitLabResource.ISSUE:
-        console.debug("Issue identified");
+        await this.renderIssueEmbed(embedParentElement, url);
         break;
       case GitLabResource.MERGE_REQUEST:
         console.debug("Merge request identified");
@@ -46,6 +47,46 @@ export default class GitLabPlugin extends Plugin {
         break;
     }
   }
+
+  private async renderIssueEmbed(element: HTMLElement, url: GitLabURL): Promise<void> {
+    console.debug(`Fetching ${BASEURL}/api/v4/projects/${url.group}%2F${url.project}/issues/${url.id}`);
+    const response = await requestUrl({ url: `${BASEURL}/api/v4/projects/${url.group}%2F${url.project}/issues/${url.id}`, method: "GET" });
+    const issue = await response.json as GitLabIssue;
+    console.debug(issue);
+
+    const embedElement = element.createEl("a"); 
+    embedElement.classList.add("gitlab-embed");
+    embedElement.classList.add("gitlab-issue");
+
+    const repoElement = embedElement.createEl("div", { text: `${url.group}/${url.project}` });
+    repoElement.classList.add("gitlab-repo");
+
+    const headingElement = embedElement.createEl("div");
+    headingElement.classList.add("gitlab-heading");
+
+    const identifierElement = headingElement.createEl("span", { text: '#' + url.id + " " });
+    identifierElement.classList.add("gitlab-identifier");
+    headingElement.appendText(issue.title);
+
+    const authorElement = embedElement.createEl("div", { text: issue.author.username });
+    authorElement.classList.add("gitlab-author");
+
+    const date = new Date(issue.created_at);
+    const yyyy = date.getFullYear();
+    const mm = String(date.getMonth() + 1).padStart(2, '0'); // months are 0-indexed
+    const dd = String(date.getDate()).padStart(2, '0');
+
+    const dateElement = embedElement.createEl("div", { text: `${yyyy}-${mm}-${dd}` });
+    dateElement.classList.add("gitlab-date");
+  }
+}
+
+type GitLabIssue = {
+  title: string;
+  author: {
+    username: string;
+  };
+  created_at: string;
 }
 
 class GitLabURL {
@@ -74,7 +115,7 @@ class GitLabURL {
     
     this.url = match[0];
     this.baseURL = match[1] as string;
-    this.group = match[2] as string;
+    this.group = (match[2] as string).slice(0, -1); // remove trailing slash
     this.project = match[3] as string;
     this.resource = match[4] as GitLabResource;
     this.id = match[5] as string;
