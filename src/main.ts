e49485f083f36d3f50f4c59f532cb17fd72fe676
Olivier Goulet <olivier_goulet@icloud.com>
2026-02-03 19:26:31 -0500

    feat: fetch issue

--- src/main.ts
+++ src/main.ts
@@ -57,34 +57,64 @@ export default class GitLabPlugin extends Plugin {
     const embedElement = element.createEl("a"); 
     embedElement.classList.add("gitlab-embed");
     embedElement.classList.add("gitlab-issue");
+    embedElement.setAttribute("href", url.url);
+    embedElement.setAttribute("target", "_blank");
+    embedElement.setAttribute("rel", "noopener nofollow");
 
     const repoElement = embedElement.createEl("div", { text: `${url.group}/${url.project}` });
     repoElement.classList.add("gitlab-repo");
 
-    const headingElement = embedElement.createEl("div");
-    headingElement.classList.add("gitlab-heading");
+    const headingElement = embedElement.createEl("div", { cls: "gitlab-heading" });
 
-    const identifierElement = headingElement.createEl("span", { text: '#' + url.id + " " });
-    identifierElement.classList.add("gitlab-identifier");
+    const identifierElement = headingElement.createEl("span", { text: '#' + url.id + " ", cls: "gitlab-identifier" });
     headingElement.appendText(issue.title);
 
-    const authorElement = embedElement.createEl("div", { text: issue.author.username });
-    authorElement.classList.add("gitlab-author");
+    const detailsElement = embedElement.createDiv({ cls: "gitlab-details" });
+
+    const authorElement = detailsElement.createEl("div", { cls: "gitlab-author" });
+    const authorAvatarElement = authorElement.createEl("img", { cls: "gitlab-author-avatar" });
+    authorAvatarElement.src = issue.author.avatar_url;
+    authorElement.appendText(issue.author.username);
 
     const date = new Date(issue.created_at);
     const yyyy = date.getFullYear();
     const mm = String(date.getMonth() + 1).padStart(2, '0'); // months are 0-indexed
     const dd = String(date.getDate()).padStart(2, '0');
 
-    const dateElement = embedElement.createEl("div", { text: `${yyyy}-${mm}-${dd}` });
-    dateElement.classList.add("gitlab-date");
+    const dateElement = detailsElement.createEl("div", { text: `${yyyy}-${mm}-${dd}`, cls: "gitlab-date" });
+
+    const labelsElement = detailsElement.createEl("div", { cls: "gitlab-labels" });
+    
+    issue.labels.slice(0, 3).forEach(label => labelsElement.createEl("div", { text: ellipsize(label, 20), cls: "gitlab-label" }));
+    // TODO add +3 more text
   }
+}
+
+/**
+ * Truncates text and appends ellipses if the character count exceeds
+ * threshold.
+ * @param str - the text to truncate
+ * @param count - the threshold
+ * @returns truncated text with ellipses or the full text if the text is
+ * smaller in length than threshold.
+ */
+const ellipsize = (str: string, count: number): string => {
+  if (str.length <= count) {
+    return str;
+  }
+
+  const ellipses = "...";
+
+  // Truncate text and append ellipses to meet the desired length.
+  return str.slice(0, count - ellipses.length).trimEnd() + ellipses;
 }
 
 type GitLabIssue = {
   title: string;
+  labels: string[];
   author: {
     username: string;
+    avatar_url: string;
   };
   created_at: string;
 }
